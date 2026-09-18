9feecc0d7582d41ae475ea3d0be9c1aaae3c8055
Sourabh Kanwade <sourabhkanwade10@gmail.com>
2026-09-18 11:08:45 +0530

    remove socket server

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -2,8 +2,7 @@
 
 import * as React from "react"
 import { Menu as MenuPrimitive } from "@base-ui/react/menu"
-
-import { cn } from "@/lib/utils"
+import { cn } from "cn"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { ArrowRight01Icon, Tick02Icon } from "@hugeicons/core-free-icons"
 
